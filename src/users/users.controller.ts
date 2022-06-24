96012f31f550357208790917fdfa8322ec8501cc
ZacLima91 <jadielsonmanoel@gmail.com>
2022-06-24 18:44:41 -0300

    feature(users): created the getAll resource

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('/users')
 export class UsersController {
   constructor(public readonly usersService: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Post()
+  create() {
+    return this.usersService.create();
+  }
 }
