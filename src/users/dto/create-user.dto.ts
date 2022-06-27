5f7054c0d54b0b48b1df3ed1b56b2e7a9d49ce88
ZacLima91 <jadielsonmanoel@gmail.com>
2022-06-26 23:25:12 -0300

    feature: finally

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'User name',
+    example: 'Zac',
+  })
   name: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'zac@email.com',
+  })
   email: string;
 }
